5e7b40b4d73dbc07a1bc6419c07e188411ae5efb
mathuramg <mmg9291@gmail.com>
2018-06-19 14:16:22 -0400

    add test site

--- app.js
+++
@@ -1,9 +0,0 @@
-var submitted=false;
-window.onload = function() {
-  console.log($('#gform'));
-  $('#gform').on('submit', function(e) {
-    console.log('submitted');
-    $('#gform *').fadeOut(2000);
-    $('#gform').prepend('Thank you for reaching out to us!');
-    });
-}
